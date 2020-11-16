60d328b01b6c39ef1b715f755dcbcc6ce29f29e2
Raj Tandel <rajtandel@hotmail.com>
2020-11-16 09:30:58 +0000

    creating server

--- createServer.js
+++ createServer.js
@@ -1,2 +1,17 @@
 //all the server data and logic
 //don't forget to export
+const e = require('express');
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+
+let server = express();
+server.use(cors());
+server.use(bodyParser.json());
+
+
+
+
+
+module.exports = server;
