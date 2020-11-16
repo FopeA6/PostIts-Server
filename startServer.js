3f2833741efe0a70ee899b9f9e88c4e6c7d079d5
Raj Tandel <rajtandel@hotmail.com>
2020-11-16 09:31:27 +0000

    starting server

--- startServer.js
+++ startServer.js
@@ -1,2 +1,7 @@
 //import from createServer
 //this will start the server
+const server = require('./createServer');
+
+const port = 3000;
+
+server.listen(port, ()=> console.log(`The server for PostIt's is now online on localhost ${port}`));
