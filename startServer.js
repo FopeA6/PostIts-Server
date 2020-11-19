757c72252238af243f15e92a3b8f60288f9ecf87
Fope <fopeayo@hotmail.com>
2020-11-19 15:26:28 +0000

    changes for deployment - start & port

--- startServer.js
+++ startServer.js
@@ -2,6 +2,6 @@
 //this will start the server
 const server = require('./createServer');
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 server.listen(port, ()=> console.log(`The server for PostIt's is now online on localhost ${port}`));
